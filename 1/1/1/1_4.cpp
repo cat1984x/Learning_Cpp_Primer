19282058e523500bf9f679b7f1e8f07d75c0fa8d
Chen Yushen <chenyushen1989@163.com>
2017-11-04 10:19:26 +0800

    1
    1

--- 1/1/1/1_4.cpp
+++ 1/1/1/1_4.cpp
@@ -2,7 +2,11 @@
 
 int main(void)
 {
-	std::cout << "Hello ,World" << std::endl;
+	int num1, num2;
+	std::cout << "请输入两个数" << std::endl;
+	std::cin >> num1 >> num2;
+	std::cout << num1 << "与" << num2 << "的积为：" << num1 * num2 << std::endl;
+	std::cin.get();
 	std::cin.get();
 	return 0;
 }
