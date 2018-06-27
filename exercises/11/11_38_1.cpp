b7ea910340fd61e95e7705f4830bc01dec12c25a
Chen Yushen <chenyushen1989@163.com>
2018-06-27 08:03:21 +0800

    11_38
    11_38

--- exercises/11/11_38_1.cpp
+++ exercises/11/11_38_1.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
 #include <string>
 #include <fstream>
 
@@ -7,14 +7,14 @@ using namespace std;
 
 int main()
 {
-	ifstream in("11_3_data.txt");
+	ifstream in("11_38_1_data.txt");
 	if(!in)
 	{
 		cout << "can't open file";
 		return -1;
 	}
 
-	map<string, size_t> word_count;
+	unordered_map<string, size_t> word_count;
 	string word;
 	while(in >> word)
 		++word_count[word];
@@ -22,6 +22,14 @@ int main()
 	for(const auto &w : word_count)
 		cout << w.first << " occurs " << w.second
 			<< ((w.second > 1) ? " times " : " time") << endl;
+
+	cout << "bucket_count: " << word_count.bucket_count() << endl;
+	cout << "max_bucket_count: " << word_count.max_bucket_count() << endl;
+	int n = word_count.bucket_count();
+	for(int i = 0; i != n ; ++i)
+		cout << "bucket_size(" << i << "): " << word_count.bucket_size(i) << endl;
+	cout << "bucket(beautiful): " << word_count.bucket("beautiful") << endl;
+	cout << "bucket(world): " << word_count.bucket("world") << endl;
 
 	return 0;
 }
