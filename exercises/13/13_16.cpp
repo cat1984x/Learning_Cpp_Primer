d62270978128b7da8811c3eee96222193c52a085
Chen Yushen <chenyushen1989@163.com>
2018-07-04 17:18:48 +0800

    13_16
    13_16

---
+++ exercises/13/13_16.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class numbered
+{
+public:
+	numbered() { mysn = seq++; }
+	numbered(const numbered &) { mysn = seq++; }
+	int mysn;
+private:
+	static int seq;
+};
+
+int numbered::seq = 0;
+
+void f(const numbered &s)
+{
+	cout << s.mysn << endl;
+}
+
+int main()
+{
+	numbered a, b = a, c = b;
+	f(a);
+	f(b);
+	f(c);
+
+	return 0;
+}
